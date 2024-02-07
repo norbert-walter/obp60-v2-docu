b27e304e968240fa29e84826c958d59003f60026
norbert-walter <norbert-walter@web.de>
2024-02-07 16:51:52 +0100

    Switch to index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,15 @@ intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # -- Options for EPUB output
 epub_show_urls = "footnote"
 
